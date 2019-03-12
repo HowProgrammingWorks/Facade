d7063a5d895840845d0df73390f3dfa6f1534d8f
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-12 11:36:50 +0200

    Add usage examples

--- JavaScript/2-class.js
+++ JavaScript/2-class.js
@@ -39,8 +39,18 @@ class TimeoutCollection {
 
 const hash = new TimeoutCollection(1000);
 hash.set('uno', 1);
+console.dir({ array: hash.toArray() });
+
+hash.set('due', 2);
+console.dir({ array: hash.toArray() });
+
 setTimeout(() => {
-  hash.set('due', 2);
   hash.set('tre', 3);
   console.dir({ array: hash.toArray() });
+
+  setTimeout(() => {
+    hash.set('quattro', 4);
+    console.dir({ array: hash.toArray() });
+  }, 500);
+
 }, 1500);
--- JavaScript/3-function.js
+++ JavaScript/3-function.js
@@ -35,8 +35,18 @@ const timeoutCollection = timeout => {
 
 const hash = timeoutCollection(1000);
 hash.set('uno', 1);
+console.dir({ array: hash.toArray() });
+
+hash.set('due', 2);
+console.dir({ array: hash.toArray() });
+
 setTimeout(() => {
-  hash.set('due', 2);
   hash.set('tre', 3);
   console.dir({ array: hash.toArray() });
+
+  setTimeout(() => {
+    hash.set('quattro', 4);
+    console.dir({ array: hash.toArray() });
+  }, 500);
+
 }, 1500);
